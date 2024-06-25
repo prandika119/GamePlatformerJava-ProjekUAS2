0758bd5f46eb78382a84a2d642c753a7b5f786f9
prandika119 <an.prasetya9@gmail.com>
2024-06-25 13:11:40 +0700

    ganti judul

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -3,16 +3,16 @@ package Main;
 import javax.swing.JFrame;
 
 public class Game {
-	
+
 	public static void main(String[] args) {
-		
-		JFrame window = new JFrame("Dragon Tale");
+
+		JFrame window = new JFrame("Mountain Adventure");
 		window.setContentPane(new GamePanel());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.pack();
 		window.setVisible(true);
-		
+
 	}
-	
+
 }
